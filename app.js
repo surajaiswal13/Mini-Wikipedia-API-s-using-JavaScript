ce88ad88ba175f9e5441d8a344b6b355db408738
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-24 21:01:49 +0530

    PUT, PATCH, DELETE [ SINGLE & ALL ]

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// GET API'switch
+app.route("/articles")
 
-app.get("/articles", (req, res) => {
+// GET API's
+
+.get((req, res) => {
   // Article.find({}, function(err, results) {
   Article.find(function(err, results) {
     if (!err) {
@@ -36,10 +38,12 @@ app.get("/articles", (req, res) => {
   })
 })
 
-app.post("/articles", (req, res) => {
+// POST API's
 
-  console.log(req.body.title);
-  console.log(req.body.content);
+.post((req, res) => {
+
+  // console.log(req.body.title);
+  // console.log(req.body.content);
   title = req.body.title;         // title is name of input field.
   content = req.body.content;     // content is name of input field.
 
@@ -48,9 +52,87 @@ app.post("/articles", (req, res) => {
     content: content,
   })
 
-  newArticle.save();
+  newArticle.save(function(err) {
+    if (!err) {
+      res.send(newArticle);
+    } else {
+      res.send(err);
+    }
+  });
 
 })
+
+// DELETE ALL API
+
+.delete(function(req, res) {
+  Article.deleteMany(function(err) {
+    if (!err) {
+      res.send({"message": "Deleted All Records from DB"});
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// ------------ SINGLE API's ---------------
+
+app.route("/articles/:articleTitle")
+
+.get((req, res) => {
+  Article.findOne({title: req.params.articleTitle}, (err, foundArticle) => {
+    if (!err) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send({"message": "No article found"});
+      }
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.put(function(req, res) {
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    // {overwrite: true},
+    function(err) {
+      if(!err) {
+        res.send("SuccessFully updated article.");
+      } else {
+        res.send(err)
+      }
+    }
+  )
+})
+
+.patch(function(req, res) {
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err) {
+      if(!err) {
+        res.send("SuccessFully Updated Article")
+      } else {
+        res.send(err)
+      }
+    }
+  );
+})
+
+.delete((req, res) => {
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err) {
+      if (!err) {
+        res.send("SuccessFully delete the corresponding article.")
+      } else {
+        res.send(err)
+      }
+    }
+  );
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
